7ccc4dcc13c4defbc35d540a1fa29139da979232
muzamilindra <muzamil.indra@gmail.com>
2023-09-21 11:08:45 +0700

    add floating action button

---
+++ lib/pages/home_page/components/item_action_floating_button.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+class ItemActionFloatingButton extends StatelessWidget {
+  const ItemActionFloatingButton({
+    required this.image,
+    required this.title,
+    required this.isActive,
+    Key? key,
+  }) : super(key: key);
+
+  final String image;
+  final String title;
+  final bool isActive;
+
+  @override
+  Widget build(BuildContext context) {
+    // Column(
+    //   mainAxisAlignment: MainAxisAlignment.center,
+    //   crossAxisAlignment: CrossAxisAlignment.center,
+    //   children: [
+    //     Image.asset(
+    //       'assets/icons/home.png',
+    //       width: 26,
+    //       color: Colors.purple,
+    //     ),
+    //     SizedBox(
+    //       height: 6,
+    //     ),
+    //     Text(
+    //       "Home",
+    //       style: TextStyle(
+    //           fontFamily: "roboto",
+    //           fontSize: 14,
+    //           fontWeight: FontWeight.w300,
+    //           fontStyle: FontStyle.italic,
+    //           color: Colors.purple.shade300),
+    //     )
+    //   ],
+    // ),
+
+    // return Column(
+    //   mainAxisAlignment: MainAxisAlignment.center,
+    //   crossAxisAlignment: CrossAxisAlignment.center,
+    //   children: [
+    //     Image.asset(
+    //       image,
+    //       width: 20,
+    //       color: Colors.grey.shade800,
+    //       // color: Colors.purple,
+
+    //     ),
+    //     SizedBox(
+    //       height: 6,
+    //     ),
+    //     Text(
+    //       title,
+    //       style: TextStyle(
+    //           fontFamily: "roboto",
+    //           fontSize: 12,
+    //           fontWeight: FontWeight.bold,
+    //           fontStyle: FontStyle.italic,
+    //           color: Colors.grey.shade800),
+    //     )
+    //   ],
+    // );
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: isActive
+            ? [
+                Image.asset(
+                  image,
+                  width: 20,
+                  color: Colors.purple,
+                  // color: Colors.purple,
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                Text(
+                  title,
+                  style: TextStyle(
+                      fontFamily: "roboto",
+                      fontSize: 14,
+                      fontWeight: FontWeight.w300,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.purple.shade300),
+                )
+              ]
+            : [
+                Image.asset(
+                  image,
+                  width: 20,
+                  color: Colors.grey.shade700,
+                  // color: Colors.purple,
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                Text(
+                  title,
+                  style: TextStyle(
+                      fontFamily: "roboto",
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.grey.shade800),
+                )
+              ]);
+  }
+}
--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -7,6 +7,7 @@ import 'package:property_rents/pages/home_page/components/next_trip.dart';
 import 'package:property_rents/themes.dart';
 
 import 'components/become_host.dart';
+import 'components/item_action_floating_button.dart';
 import 'components/near_locations.dart';
 import 'components/top_rated_city.dart';
 import 'components/travel_tips.dart';
@@ -59,181 +60,229 @@ class _HomePageState extends State<HomePage>
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: whiteColor,
-      body: SingleChildScrollView(
-        // physics: const NeverScrollableScrollPhysics(),
-        physics: const BouncingScrollPhysics(),
-        child: Padding(
-          padding: EdgeInsets.only(
-              left: rlEdge, top: rlEdge + 20, bottom: rlEdge, right: rlEdge),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Find your place in",
-                style: greyBoldTextStyle.copyWith(
-                    fontFamily: 'roboto',
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                    wordSpacing: 0.1),
-              ),
-              const SizedBox(
-                height: 8,
-              ),
-              Row(
-                children: [
-                  Image.asset(
-                    "assets/icons/location.png",
-                    width: 20,
-                    height: 20,
-                  ),
-                  const SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    "Surabaya, Indonesia",
-                    style: greyBoldTextStyle.copyWith(
-                        fontFamily: "roboto",
-                        fontSize: 20,
-                        fontWeight: FontWeight.w200,
-                        fontStyle: FontStyle.italic),
-                  ),
-                  const SizedBox(
-                    width: 8,
-                  ),
-                  Image.asset(
-                    "assets/icons/arrow-down.png",
-                    width: 20,
-                    height: 20,
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 25,
-              ),
-              SizedBox(
-                height: 48,
-                child: MyTextField(
-                  type: "text",
-                  focus: _focusOne,
-                  focused: focusedOne,
-                  prefixIcon: "assets/icons/search-normal.png",
-                  suffixIcon: "assets/icons/setting.png",
-                  obscuring: false,
+      body: SafeArea(
+        minimum: EdgeInsets.only(top: 0, bottom: 80),
+        child: SingleChildScrollView(
+          // physics: const NeverScrollableScrollPhysics(),
+          physics: const BouncingScrollPhysics(),
+          child: Padding(
+            padding: EdgeInsets.only(
+                left: rlEdge, top: rlEdge + 20, bottom: rlEdge, right: rlEdge),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Find your place in",
+                  style: greyBoldTextStyle.copyWith(
+                      fontFamily: 'roboto',
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                      wordSpacing: 0.1),
                 ),
-              ),
-              const SizedBox(
-                height: 36,
-              ),
-              Text(
-                "What dou you need?",
-                style: greyBoldTextStyle.copyWith(
-                    fontFamily: "roboto",
-                    fontSize: 18,
-                    fontWeight: FontWeight.w200,
-                    fontStyle: FontStyle.italic),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              DefaultTabController(
-                length: 2,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                const SizedBox(
+                  height: 8,
+                ),
+                Row(
                   children: [
-                    Container(
-                      height: 53,
-                      // color: Color(0xFFE3E3E7),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: const Color(0xFFE3E3E7)),
-                        borderRadius: BorderRadius.circular(72),
-                        color: const Color(0xffF5F4F8),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: TabBar(
-                          indicator: BoxDecoration(
-                              boxShadow: [
-                                BoxShadow(
-                                  spreadRadius: -8,
-                                  color:
-                                      const Color(0xFF917AFD).withOpacity(0.5),
-                                  blurRadius: 12,
-                                  offset: const Offset(
-                                      0, 10), // changes position of shadow
-                                ),
-                              ],
-                              gradient: const LinearGradient(
-                                  begin: Alignment.topLeft,
-                                  end: Alignment.bottomRight,
-                                  stops: [
-                                    0.0,
-                                    1
-                                  ],
-                                  colors: [
-                                    Color(0xFF917AFD),
-                                    Color(0xFF6246EA),
-                                  ]),
-                              borderRadius: BorderRadius.circular(72)),
-                          // labelColor: Colors.black,
-                          labelColor: Colors.white,
-                          labelStyle: whiteTextStyle.copyWith(
-                              fontFamily: 'roboto',
-                              fontSize: 15,
-                              fontWeight: FontWeight.w500),
-                          // indicatorColor: whiteColor,
-                          unselectedLabelColor: greyBoldColor,
-                          unselectedLabelStyle: greyBoldTextStyle.copyWith(
-                              fontFamily: 'roboto',
-                              fontSize: 15,
-                              fontWeight: FontWeight.w300),
-                          onTap: (index) {},
-                          controller: _controller,
-                          tabs: const [
-                            Tab(
-                              text: "I need to rent",
-                            ),
-                            Tab(
-                              text: "I need to buy",
-                            ),
-                          ],
-                        ),
-                      ),
+                    Image.asset(
+                      "assets/icons/location.png",
+                      width: 20,
+                      height: 20,
                     ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      "Surabaya, Indonesia",
+                      style: greyBoldTextStyle.copyWith(
+                          fontFamily: "roboto",
+                          fontSize: 20,
+                          fontWeight: FontWeight.w200,
+                          fontStyle: FontStyle.italic),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Image.asset(
+                      "assets/icons/arrow-down.png",
+                      width: 20,
+                      height: 20,
+                    )
                   ],
                 ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              const NearLocations(),
-              const SizedBox(
-                height: 36,
-              ),
-              const TopRatedCity(),
-              const SizedBox(
-                height: 36,
-              ),
-              const NextTrip(),
-              const SizedBox(
-                height: 36,
-              ),
-              const TravelTips(),
-              const SizedBox(
-                height: 36,
-              ),
-              const ThirdButton(
-                text: "Read more article",
-              ),
-              const SizedBox(
-                height: 36,
-              ),
-              const BecomeHost()
-            ],
+                const SizedBox(
+                  height: 25,
+                ),
+                SizedBox(
+                  height: 48,
+                  child: MyTextField(
+                    type: "text",
+                    focus: _focusOne,
+                    focused: focusedOne,
+                    prefixIcon: "assets/icons/search-normal.png",
+                    suffixIcon: "assets/icons/setting.png",
+                    obscuring: false,
+                  ),
+                ),
+                const SizedBox(
+                  height: 36,
+                ),
+                Text(
+                  "What dou you need?",
+                  style: greyBoldTextStyle.copyWith(
+                      fontFamily: "roboto",
+                      fontSize: 18,
+                      fontWeight: FontWeight.w200,
+                      fontStyle: FontStyle.italic),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                DefaultTabController(
+                  length: 2,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 53,
+                        // color: Color(0xFFE3E3E7),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: const Color(0xFFE3E3E7)),
+                          borderRadius: BorderRadius.circular(72),
+                          color: const Color(0xffF5F4F8),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TabBar(
+                            indicator: BoxDecoration(
+                                boxShadow: [
+                                  BoxShadow(
+                                    spreadRadius: -8,
+                                    color: const Color(0xFF917AFD)
+                                        .withOpacity(0.5),
+                                    blurRadius: 12,
+                                    offset: const Offset(
+                                        0, 10), // changes position of shadow
+                                  ),
+                                ],
+                                gradient: const LinearGradient(
+                                    begin: Alignment.topLeft,
+                                    end: Alignment.bottomRight,
+                                    stops: [
+                                      0.0,
+                                      1
+                                    ],
+                                    colors: [
+                                      Color(0xFF917AFD),
+                                      Color(0xFF6246EA),
+                                    ]),
+                                borderRadius: BorderRadius.circular(72)),
+                            // labelColor: Colors.black,
+                            labelColor: Colors.white,
+                            labelStyle: whiteTextStyle.copyWith(
+                                fontFamily: 'roboto',
+                                fontSize: 15,
+                                fontWeight: FontWeight.w500),
+                            // indicatorColor: whiteColor,
+                            unselectedLabelColor: greyBoldColor,
+                            unselectedLabelStyle: greyBoldTextStyle.copyWith(
+                                fontFamily: 'roboto',
+                                fontSize: 15,
+                                fontWeight: FontWeight.w300),
+                            onTap: (index) {},
+                            controller: _controller,
+                            tabs: const [
+                              Tab(
+                                text: "I need to rent",
+                              ),
+                              Tab(
+                                text: "I need to buy",
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                const NearLocations(),
+                const SizedBox(
+                  height: 36,
+                ),
+                const TopRatedCity(),
+                const SizedBox(
+                  height: 36,
+                ),
+                const NextTrip(),
+                const SizedBox(
+                  height: 36,
+                ),
+                const TravelTips(),
+                const SizedBox(
+                  height: 36,
+                ),
+                const ThirdButton(
+                  text: "Read more article",
+                ),
+                const SizedBox(
+                  height: 36,
+                ),
+                const BecomeHost()
+              ],
+            ),
           ),
         ),
       ),
+      floatingActionButton: Container(
+        // color: Colors.amber.shade300,
+        width: MediaQuery.of(context).size.width - 20,
+        height: 70,
+        decoration: BoxDecoration(
+          // borderRadius: BorderRadius.only(
+          //     topLeft: Radius.circular(30), topRight: Radius.circular(30)),
+           borderRadius: BorderRadius.circular(50),
+          color: Colors.white,
+          // color: Colors.amber.shade300,
+        ),
+        // margin: EdgeInsets.symmetric(horizontal: 10),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: const [
+            ItemActionFloatingButton(
+              image: 'assets/icons/home.png',
+              title: "Home",
+              isActive: true,
+            ),
+            ItemActionFloatingButton(
+              image: 'assets/icons/signpost.png',
+              title: "Explorer",
+              isActive: false,
+            ),
+            ItemActionFloatingButton(
+              image: 'assets/icons/message-text.png',
+              title: "Chat",
+              isActive: false,
+            ),
+            ItemActionFloatingButton(
+              image: 'assets/icons/heart.png',
+              title: "Saved",
+              isActive: false,
+            ),
+            ItemActionFloatingButton(
+              image: 'assets/icons/profile.png',
+              title: "Profile",
+              isActive: false,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
