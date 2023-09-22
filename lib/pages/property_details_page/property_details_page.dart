d7ef2ad5a165f0124095195e261958b6a62b6ad3
muzamilindra <muzamil.indra@gmail.com>
2023-09-22 16:00:51 +0700

    add image slider

--- lib/pages/property_details_page/property_details_page.dart
+++ lib/pages/property_details_page/property_details_page.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:property_rents/themes.dart';
+import 'package:video_player/video_player.dart';
 
-class PropertyDetailsPage extends StatelessWidget {
-  const PropertyDetailsPage({Key? key}) : super(key: key);
+class PropertyDetailsPage extends StatefulWidget {
+  PropertyDetailsPage({Key? key}) : super(key: key);
+
+  static final List<String> imgList = [
+    'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+    'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+    'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+    'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+    'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+    'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+  ];
+
+  @override
+  State<PropertyDetailsPage> createState() => _PropertyDetailsPageState();
+}
+
+class _PropertyDetailsPageState extends State<PropertyDetailsPage> {
+  late String indexImage = '1';
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +29,95 @@ class PropertyDetailsPage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            
+            Stack(children: [
+              CarouselSlider(
+                items: PropertyDetailsPage.imgList
+                    .asMap()
+                    .entries
+                    .map((fileImage) {
+                  return Image.network(
+                    fileImage.value,
+                    fit: BoxFit.cover,
+                  );
+                }).toList(),
+                options: CarouselOptions(
+                  pageSnapping: true,
+                  viewportFraction: 1,
+                  height: 269,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      indexImage = (index + 1).toString();
+                    });
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 46, left: 16, right: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          color: Colors.white),
+                      child: IconButton(
+                          splashRadius: 20.0,
+                          padding: const EdgeInsets.all(0.0),
+                          constraints: const BoxConstraints(),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: Icon(
+                            Icons.arrow_back_ios_rounded,
+                            size: 18,
+                            color: blueDarkColor,
+                          )),
+                    ),
+                    Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          color: Colors.white),
+                      child: IconButton(
+                          splashRadius: 20.0,
+                          padding: const EdgeInsets.all(0.0),
+                          constraints: const BoxConstraints(),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: Icon(
+                            Icons.all_inclusive_rounded,
+                            size: 18,
+                            color: blueDarkColor,
+                          )),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                bottom: 10,
+                right: 10,
+                child: Container(
+                  width: 58,
+                  height: 24,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      color: Colors.white),
+                  child: Center(
+                    child: Text(
+                      '$indexImage / ' +
+                          PropertyDetailsPage.imgList.length.toString(),
+                      style: blueDarkTextStyle.copyWith(
+                          fontFamily: "roboto", fontSize: 14),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              )
+            ])
           ],
         ),
       ),
